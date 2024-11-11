5cbdef4218ca894dc62775cfad2af26f30a405b6
Om Mishra <145644657+OmMishra16@users.noreply.github.com>
2024-11-11 13:42:41 +0530

    Added CORS in main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes.chat import router as chat_router
 
 app = FastAPI(title="Chatbot API", version="1.0.0")
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://fast-api-chat-bot-frontend-j5nwl9vu6.vercel.app", "http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(chat_router, prefix="/api/chat", tags=["chat"])
 
